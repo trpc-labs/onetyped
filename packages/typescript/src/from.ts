2aab973ea8d4e155962c91bb10be2e57f5f71c93
Sachin Raja <me@s4n.land>
2022-11-13 12:24:31 -0800

    fix ts tuple and function

--- packages/typescript/src/from.ts
+++ packages/typescript/src/from.ts
@@ -19,34 +19,34 @@ import {
 	union,
 	unknown,
 } from '@onetyped/core'
-import { Node, ObjectFlags, ObjectType, SymbolFlags, Type, TypeChecker, TypeFlags, TypeReference } from 'typescript'
+import ts, { ObjectType } from 'typescript'
 
-const hasFlag = (type: Type, flag: TypeFlags): boolean => {
-	return (type.flags & flag) !== 0
+const hasFlag = (type: ts.Type, flag: ts.TypeFlags): boolean => {
+	return (type.flags & flag) === flag
 }
 
-const hasSymbolFlag = (type: Type, flag: SymbolFlags): boolean => {
-	return (type.flags & flag) !== 0
+const hasSymbolFlag = (symbol: ts.Symbol, flag: ts.SymbolFlags): boolean => {
+	return (symbol.flags & flag) === flag
 }
 
-const hasObjectFlag = (type: ObjectType, flag: ObjectFlags): boolean => {
-	return (type.objectFlags & flag) !== 0
+const hasObjectFlag = (type: ts.ObjectType, flag: ts.ObjectFlags): boolean => {
+	return (type.objectFlags & flag) === flag
 }
 
-export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): AnyNode => {
-	if (hasFlag(type, TypeFlags.String)) {
+export const fromType = (type: ts.Type, locationNode: ts.Node, checker: ts.TypeChecker): AnyNode => {
+	if (hasFlag(type, ts.TypeFlags.String)) {
 		return string()
 	}
 
-	if (hasFlag(type, TypeFlags.Number)) {
+	if (hasFlag(type, ts.TypeFlags.Number)) {
 		return number()
 	}
 
-	if (hasFlag(type, TypeFlags.Boolean)) {
+	if (hasFlag(type, ts.TypeFlags.Boolean)) {
 		return boolean()
 	}
 
-	if (hasFlag(type, TypeFlags.Unknown)) {
+	if (hasFlag(type, ts.TypeFlags.Unknown)) {
 		return unknown()
 	}
 
@@ -59,15 +59,47 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 		return literal(type.value)
 	}
 
-	if (hasFlag(type, TypeFlags.Any)) {
+	if (hasFlag(type, ts.TypeFlags.Any)) {
 		return any()
 	}
 
-	if (hasFlag(type, TypeFlags.BigInt)) {
+	if (hasFlag(type, ts.TypeFlags.BigInt)) {
 		return bigint()
 	}
 
-	if (hasFlag(type, TypeFlags.Object)) {
+	if (hasFlag(type, ts.TypeFlags.Object)) {
+		const objectType = type as ObjectType
+		console.log(checker.typeToString(type))
+		if (hasObjectFlag(objectType, ts.ObjectFlags.Reference)) {
+			const { target } = (type as ts.TypeReference)
+
+			if (hasObjectFlag(target, ts.ObjectFlags.Tuple)) {
+				const tupleTypes = checker.getTypeArguments(type as ts.TypeReference).map((tupleType) =>
+					fromType(tupleType, locationNode, checker)
+				) as [AnyNode, ...AnyNode[]]
+				return tuple(tupleTypes)
+			}
+		}
+
+		const callSignatures = type.getCallSignatures()
+
+		if (callSignatures.length > 0) {
+			const nodeCallSignatures = callSignatures.map((signature) => {
+				const parameters = signature.getParameters().map((parameter) => {
+					const parameterType = checker.getTypeOfSymbolAtLocation(parameter, locationNode)
+					return fromType(parameterType, locationNode, checker)
+				})
+				const returnType = checker.getReturnTypeOfSignature(signature)
+
+				return func({
+					arguments: parameters as any,
+					return: fromType(returnType, locationNode, checker),
+				})
+			}) as [AnyNode, ...AnyNode[]]
+
+			return union(nodeCallSignatures)
+		}
+
 		const propertySchemas = Object.fromEntries(
 			type.getProperties().map((
 				property,
@@ -79,12 +111,13 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 		return object(propertySchemas)
 	}
 
-	const symbolName = type.symbol.name
-	if (symbolName) {
-		switch (symbolName) {
+	const symbol = type.getSymbol()
+	if (symbol) {
+		// console.log(symbol.getName())
+		switch (symbol.name) {
 			case 'Array':
 			case 'ReadonlyArray': {
-				const typeArguments = checker.getTypeArguments(type as TypeReference)
+				const typeArguments = checker.getTypeArguments(type as ts.TypeReference)
 				return array(fromType(typeArguments[0], locationNode, checker))
 			}
 
@@ -93,19 +126,19 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 			}
 
 			case 'Set': {
-				const typeArguments = checker.getTypeArguments(type as TypeReference)
+				const typeArguments = checker.getTypeArguments(type as ts.TypeReference)
 				return set(fromType(typeArguments[0], locationNode, checker))
 			}
 
 			case 'Record': {
-				const typeArguments = checker.getTypeArguments(type as TypeReference)
+				const typeArguments = checker.getTypeArguments(type as ts.TypeReference)
 				return record(
 					fromType(typeArguments[1], locationNode, checker),
 				)
 			}
 
 			case 'Map': {
-				const typeArguments = checker.getTypeArguments(type as TypeReference)
+				const typeArguments = checker.getTypeArguments(type as ts.TypeReference)
 				return map(
 					fromType(typeArguments[0], locationNode, checker),
 					fromType(typeArguments[1], locationNode, checker),
@@ -113,7 +146,7 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 			}
 		}
 
-		throw new Error(`Unknown symbol name: ${symbolName}`)
+		throw new Error(`Unknown symbol name: ${symbol.name}`)
 	}
 
 	if (type.isUnion()) {
@@ -124,13 +157,6 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 		return union(unionTypes)
 	}
 
-	if (hasObjectFlag(type as ObjectType, ObjectFlags.Tuple)) {
-		const tupleTypes = checker.getTypeArguments(type as TypeReference).map((tupleType) =>
-			fromType(tupleType, locationNode, checker)
-		) as [AnyNode, ...AnyNode[]]
-		return tuple(tupleTypes)
-	}
-
 	if (type.isIntersection()) {
 		const intersectionTypes = type.types.map((type) => fromType(type, locationNode, checker)) as [
 			AnyNode,
@@ -139,26 +165,69 @@ export const fromType = (type: Type, locationNode: Node, checker: TypeChecker): 
 		return intersection(intersectionTypes)
 	}
 
-	if (hasSymbolFlag(type, SymbolFlags.Optional)) {
+	if (hasSymbolFlag(type, ts.SymbolFlags.Optional)) {
 		return optional(number())
-	}
-
-	if (hasSymbolFlag(type, SymbolFlags.Function)) {
-		const callSignatures = type.getCallSignatures().map((signature) => {
-			const parameters = signature.getParameters().map((parameter) => {
-				const parameterType = checker.getTypeOfSymbolAtLocation(parameter, locationNode)
-				return fromType(parameterType, locationNode, checker)
-			})
-			const returnType = checker.getReturnTypeOfSignature(signature)
-
-			return func({
-				arguments: parameters as any,
-				return: fromType(returnType, locationNode, checker),
-			})
-		}) as [AnyNode, ...AnyNode[]]
-
-		return union(callSignatures)
 	}
 
 	throw new Error(`Unknown type: ${checker.typeToString(type)}`)
 }
+
+const filename = 'test.ts'
+const code = `
+type User = {
+	name: string & { length: number }
+	age: number
+	func: (a: number, b: string) => string
+	role: 'admin' | 'user'
+	t: [string, number]
+}
+const user = undefined as unknown as User`
+
+const sourceFile = ts.createSourceFile(
+	filename,
+	code,
+	ts.ScriptTarget.Latest,
+)
+
+const defaultCompilerHost = ts.createCompilerHost({})
+
+const customCompilerHost: ts.CompilerHost = {
+	getSourceFile: (name, languageVersion) => {
+		console.log(`getSourceFile ${name}`)
+
+		return name === filename ? sourceFile : defaultCompilerHost.getSourceFile(
+			name,
+			languageVersion,
+		)
+	},
+	writeFile: (filename, data) => {},
+	getDefaultLibFileName: () => 'lib.d.ts',
+	useCaseSensitiveFileNames: () => false,
+	getCanonicalFileName: filename => filename,
+	getCurrentDirectory: () => '',
+	getNewLine: () => '\n',
+	getDirectories: () => [],
+	fileExists: () => true,
+	readFile: () => '',
+}
+
+const program = ts.createProgram([filename], {}, customCompilerHost)
+
+const typeChecker = program.getTypeChecker()
+
+function recursivelyPrintVariableDeclarations(
+	node: ts.Node,
+	sourceFile: ts.SourceFile,
+) {
+	if (node.kind === ts.SyntaxKind.VariableDeclaration) {
+		const type = typeChecker.getTypeAtLocation(node)
+
+		const ourNode = fromType(type, sourceFile, typeChecker)
+
+		console.log(ourNode)
+	}
+
+	node.forEachChild(child => recursivelyPrintVariableDeclarations(child, sourceFile))
+}
+
+recursivelyPrintVariableDeclarations(sourceFile, sourceFile)
